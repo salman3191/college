c3fd876d8d96733daefb2dba968a5ea62cfc73c2
M salman khan <mohammadsalmankhan3191@gmail.com>
2024-09-07 21:21:30 +0530

    solved a question based on switch statement

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-let strng = "A big dream";
-if (strng[0] === "a" && strng.length > 3) {
-  console.log("good string");
-} else {
-  console.log("bad string");
+let day = 1;
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("tuesday");
+    break;
+  case 3:
+    console.log("wednesday");
+    break;
+  case 4:
+    console.log("thursday");
+    break;
+  case 5:
+    console.log("friday");
+    break;
+  case 6:
+    console.log("saturday");
+    break;
+  case 7:
+    console.log("sunday");
+    break;
+  default:
+    console.log("not a valid day");
 }
